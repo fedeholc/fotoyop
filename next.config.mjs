8514e16a6274d16961859ed6eab3b7b7bea57d36
fedeholc <federicoholc@gmail.com>
2024-02-19 07:58:48 -0300

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/* const nextConfig = {};
 
-export default nextConfig;
+export default nextConfig; */
+
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist({
+  // Your Next.js config
+});
