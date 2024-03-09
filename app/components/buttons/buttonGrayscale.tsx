25a79f69884fff7f179e479b512068318e4bbda7
fedeholc <federicoholc@gmail.com>
2024-03-09 11:13:52 -0300

    Fix enable-background attribute in ButtonGrayscale component

--- app/components/buttons/buttonGrayscale.tsx
+++ app/components/buttons/buttonGrayscale.tsx
@@ -16,7 +16,7 @@ export default function ButtonGrayscale({ onClick }: { onClick: () => void }) {
         version="1.1"
         id="Layer_1"
         viewBox="0 0 512 512"
-        enable-background="new 0 0 512 512"
+        enableBackground="new 0 0 512 512"
       >
         <path d="M256,0C114.6,0,0,114.6,0,256s114.6,256,256,256s256-114.6,256-256S397.4,0,256,0z M256,469.3V42.7  c117.8,0,213.3,95.5,213.3,213.3C469.3,373.8,373.8,469.3,256,469.3z" />
       </svg>
