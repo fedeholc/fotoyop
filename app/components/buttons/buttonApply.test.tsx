959d5433e20b95d1c446d17c67884bd5e0b31c69
fedeholc <federicoholc@gmail.com>
2024-03-31 14:46:49 -0300

    Update button click event handler in ButtonApply test

--- app/components/buttons/buttonApply.test.tsx
+++ app/components/buttons/buttonApply.test.tsx
@@ -15,7 +15,9 @@ describe("ButtonApply", () => {
   test("on click", async () => {
     const onChange = vi.fn();
     render(<ButtonApply onClick={onChange} />);
-    await userEvent.click(screen.getByRole("button"));
+    await userEvent.click(screen.getByRole("button", { name: /apply/i }));
+    //ojo, acá apply no se busca en la propiedad name del boton sino en lo que se llama el nombre accesible, el texto puede estar en el aria-label, title, etc.
+    //en este caso que hay un solo boton en el documento, se podría buscar por role y no hace falta especificar el nombre
     expect(onChange).toHaveBeenCalledTimes(1);
   });
 });
