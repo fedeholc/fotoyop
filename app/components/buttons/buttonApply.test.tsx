368b1c49348c4d28f371728edc0e8baad82b54d5
fedeholc <federicoholc@gmail.com>
2024-03-31 11:11:54 -0300

    Add user event test for ButtonApply component

--- app/components/buttons/buttonApply.test.tsx
+++ app/components/buttons/buttonApply.test.tsx
@@ -2,14 +2,20 @@ import { describe } from "node:test";
 import ButtonApply from "./buttonApply";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { test, expect } from "vitest";
+import { test, expect, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
 
 describe("ButtonApply", () => {
   test("renders", () => {
     render(<ButtonApply onClick={() => {}} />);
-
-    screen.debug();
+    //screen.debug();
     expect(screen.getByRole("button")).toBeInTheDocument();
     expect(screen.getByTitle(/Apply/i)).toBeInTheDocument();
+  });
+  test("on click", async () => {
+    const onChange = vi.fn();
+    render(<ButtonApply onClick={onChange} />);
+    await userEvent.click(screen.getByRole("button"));
+    expect(onChange).toHaveBeenCalledTimes(1);
   });
 });
