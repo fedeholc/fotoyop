9710b60ed8e279499c80e39ec1e807ef1c0eeecf
fedeholc <federicoholc@gmail.com>
2024-04-15 11:40:04 -0300

    fix playwright test

--- tests/canvas.spec.tsx
+++ tests/canvas.spec.tsx
@@ -1,9 +1,14 @@
 import { test, expect } from '@playwright/test';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 
 test('carga de una imagen', async ({ page }) => {
-  await page.goto('http://localhost:3012/');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  await page.goto('http://localhost:3013/');
   const canvas = page.locator('#canvas-small');
   const input = page.getByTestId('inputUpload');
   await expect(canvas).not.toBeVisible();
@@ -11,4 +16,5 @@ test('carga de una imagen', async ({ page }) => {
   await input.setInputFiles(path.join(__dirname, 'a.png'));
   await expect(canvas).toBeVisible();
 }
+
 );
