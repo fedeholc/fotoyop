f3016b28a9b7c809fabca12ae5a7f0a3d0b9caf5
fedeholc <federicoholc@gmail.com>
2024-04-15 11:57:29 -0300

    Update Playwright dependencies to version 1.43.1

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     setupFiles: './__tests__/setup.js',
 
     environment: 'jsdom',
-    exclude: ['**/tests/*spec**', '**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'],
+    exclude: ['**/e2e/*spec**', '**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'],
   },
 
 })
