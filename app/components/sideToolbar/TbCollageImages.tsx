a5e1169e2e47d8344d43a76e12b7d449de24b48e
Federico Holc <federicoholc@gmail.com>
2024-05-19 21:01:37 -0300

    add collage reorder on sidetoolbar

--- app/components/sideToolbar/TbCollageImages.tsx
+++ app/components/sideToolbar/TbCollageImages.tsx
@@ -3,18 +3,60 @@ import { ImageContext } from "../../providers/ImageProvider";
 import sideToolbar from "./sideToolbar.module.css";
 import ToolbarGroup from "./ToolbarGroup";
 import collage from "../CollageCanvas.module.css";
+import { ArrowUp, ArrowDown } from "lucide-react";
+import toolbar from "../BottomToolbar.module.css";
 
 export default function TbCollageImages() {
-  const { collageImages } = useContext(ImageContext);
+  const { collageImages, setCollageImages, setCollageFiles, collageFiles } =
+    useContext(ImageContext);
+
+  function handleMoveUp(index: number) {
+    const newImages = [...collageImages!];
+    const aux = newImages[index];
+    newImages[index] = newImages[index - 1];
+    newImages[index - 1] = aux;
+    setCollageImages(newImages);
+  }
+  function handleMoveDown(index: number) {
+    const newImages = [...collageImages!];
+    const aux = newImages[index];
+    newImages[index] = newImages[index + 1];
+    newImages[index + 1] = aux;
+    setCollageImages(newImages);
+  }
+
   return (
     <ToolbarGroup closedRendering={false} groupTitle="Collage Images">
       <div className={`${collage.imagesGroup} ${sideToolbar.toolbarRow}`}>
         <div className={collage.imagesList}>
           {collageImages &&
-            collageImages.map((image) => {
+            collageImages.map((image, index) => {
               return (
-                <span key={image.src}>
+                <span
+                  className={collage.imagesListRow}
+                  key={`${image.src}${index}`}
+                >
                   <img src={image.src}></img>
+                  <div className={collage.imagesListButtons}>
+                    {index > 0 && (
+                      <button
+                        title="Move Up"
+                        className={toolbar.buttonOnlyIcon}
+                        onClick={() => handleMoveUp(index)}
+                      >
+                        <ArrowUp></ArrowUp>
+                      </button>
+                    )}
+                    {index < collageImages.length - 1 && (
+                      <button
+                        title="Move Down"
+                        className={toolbar.buttonOnlyIcon}
+                        onClick={() => handleMoveDown(index)}
+                      >
+                        <ArrowDown></ArrowDown>
+                      </button>
+                    )}
+                  </div>
                 </span>
               );
             })}
